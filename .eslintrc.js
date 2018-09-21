1d2bdd60ceaf5f48d04f79330ba5744358e13c5c
Denny Tek <tek.denny@gmail.com>
2018-09-21 07:44:50 -0400

    Update test tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,12 @@ module.exports = {
     commonjs: true,
     es6: true,
     jquery: true
+  },
+  plugins: ['smells'],
+  rules: {
+    'smells/no-switch': 1,
+    'smells/no-complex-switch-case': 1,
+    'smells/no-setinterval': 1,
+    'smells/no-this-assign': 1
   }
 };
